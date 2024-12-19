25b7a73a4832eca5bf53bc5be408f23ed780bd82
Giovanni <dibellogio@gmail.com>
2024-12-19 13:53:37 +0100

    fix second ex in arrays bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -17,16 +17,7 @@ console.log(reversedTeachers);
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 
-const longNames = [];
-
-for (let i = 0; i < teachers.length; i++) {
-
-  if (teachers[i].length >= 5) {
-    longNames.push(teachers[i])
-  }
-
-}
-
+const longNames = teachers.filter((teachers) => teachers.length > 4);
 console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
